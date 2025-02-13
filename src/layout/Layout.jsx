0a81ec9dc574c7627e53ce663865d4491d08a045
EstefaniaDelgado <teff_64@hotmail.com>
2025-02-13 17:07:35 -0500

    refactor(layout): add paddings

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,17 +4,11 @@ import Footer from '../components/Footer';
 const Layout = () => {
   return (
     <div className="mx-auto min-h-screen flex flex-col">
-      <Navbar/>
-      <main className="flex-1 bg-yellow-400">
+      <Navbar />
+      <main className="flex-1 flex flex-col pt-[96px]">
         <Outlet />
       </main>
-      <Footer/>
-      <p className='border-b-4'>NAVBAR</p>
-      <main className="flex-1 flex flex-col">
-        <Outlet />
-      </main>
-      <p className='border-t-4'>FOOTER</p>
-main
+      <Footer />
     </div>
   );
 };
