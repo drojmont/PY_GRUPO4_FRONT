f3451f7ff10efef126212380099a5b4f64b37a23
EstefaniaDelgado <teff_64@hotmail.com>
2025-02-13 16:37:32 -0500

    feature(tailwind config): add palette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
+const withMT = require('@material-tailwind/react/utils/withMT');
 
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
 module.exports = withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        jet: '#353535',
+        'dark-slate-gray': '#395253',
+        'caribean-current': '#3C6E71',
+        'ash-gray': '#9EB7B8',
+        'custom-white': '#FFFFFF',
+        'anti-flash-white': '#ECECEC',
+        'platinum-one': '#E3E3E3',
+        'platinum-two': '#D9D9D9',
+        'cadet-gray': '#81929E',
+        'indigo-dye': '#284B63',
+      },
+    },
   },
   plugins: [],
 });
