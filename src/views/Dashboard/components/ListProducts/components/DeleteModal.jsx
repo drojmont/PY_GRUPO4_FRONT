771f92674f2666c429b4089b897dfd93900746ed
EstefaniaDelgado <teff_64@hotmail.com>
2025-02-16 10:49:17 -0500

    feature(DeleteProduct): add function to delete

--- src/views/Dashboard/components/ListProducts/components/DeleteModal.jsx
+++ src/views/Dashboard/components/ListProducts/components/DeleteModal.jsx
@@ -4,15 +4,29 @@ import {
   DialogFooter,
   Button,
 } from '@material-tailwind/react';
+import useEvents from '../../../../../Hooks/useEvents';
 
-const DeleteModal = ({ open, onClose }) => {
+
+const DeleteModal = ({ open, onClose, eventId, setOpenModal }) => {
+
+  const { deleteEvents } = useEvents();
+
+  const handleDeleteEvent = () => {
+    deleteEvents(eventId)
+    setOpenModal(false);
+  };
+
   return (
     <Dialog open={open} handler={onClose} size="xs" className="bg-[#CEE7E2]">
       <DialogBody className="text-center py-5">
         <p>¿Desea Eliminar el producto?</p>
       </DialogBody>
       <DialogFooter className="flex justify-center gap-2 font-light">
-        <Button color="blue" className="rounded-md capitalize font-light">
+        <Button
+          color="blue"
+          className="rounded-md capitalize font-light"
+          onClick={handleDeleteEvent}
+        >
           Si
         </Button>
         <Button
