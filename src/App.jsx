1b44065c477065897e7e0b4d5e6a1a7c72699e22
EstefaniaDelgado <teff_64@hotmail.com>
2025-03-05 18:10:23 -0500

    refactor(protectRoute): change redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { Route, Routes } from "react-router";
-import Layout from "./layout/Layout";
-import Home from "./views/Home/Home";
-import Detail from "./Routes/Detail";
-import Dashboard from "./views/Dashboard/Dashboard";
-import AddProducts from "./views/Dashboard/components/AddProduct/AddProducts";
-import CreateProduct from "./views/Dashboard/components/AddProduct/components/CreateProduct";
-import ListProducts from "./views/Dashboard/components/ListProducts/ListProducts";
-import Register from "./views/Register";
-import EditProduct from "./views/Dashboard/components/AddProduct/components/EditProduct";
+import { Route, Routes } from 'react-router';
+import Layout from './layout/Layout';
+import Home from './views/Home/Home';
+import Detail from './Routes/Detail';
+import Dashboard from './views/Dashboard/Dashboard';
+import AddProducts from './views/Dashboard/components/AddProduct/AddProducts';
+import CreateProduct from './views/Dashboard/components/AddProduct/components/CreateProduct';
+import ListProducts from './views/Dashboard/components/ListProducts/ListProducts';
+import Register from './views/Register';
+import EditProduct from './views/Dashboard/components/AddProduct/components/EditProduct';
 import ListUsers from './views/Dashboard/components/ListUsers/ListUsers';
 
-import Features from "./views/Dashboard/components/ListFeatures/Features";
-import EditFeature from "./views/Dashboard/components/ListFeatures/components/EditFeature";
-import AddFeature from "./views/Dashboard/components/ListFeatures/components/AddFeature";
+import Features from './views/Dashboard/components/ListFeatures/Features';
+import EditFeature from './views/Dashboard/components/ListFeatures/components/EditFeature';
+import AddFeature from './views/Dashboard/components/ListFeatures/components/AddFeature';
 
-import Login from "./views/Auth/Login";
-import Category from "./Routes/Catogory";
+import Login from './views/Auth/Login';
+import Category from './Routes/Catogory';
 
-import CategoryForm from "./components/CategoryForm";
-import CategoryList from "./components/CategoryList";
+import CategoryForm from './components/CategoryForm';
+import CategoryList from './components/CategoryList';
+import ProtectedRoute from './views/Auth/ProtectRoute';
 
 function App() {
   return (
@@ -31,20 +32,24 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* Panel Administrador */}
-        <Route path="/administracion" element={<Dashboard />}>
-          <Route index element={<AddProducts />} />
-          <Route path="crear-producto" element={<CreateProduct />} />
-          <Route path="listar-productos" element={<ListProducts />} />
-          <Route path="editar-producto/:id" element={<EditProduct />} />
-          <Route path="caracteristicas" element={<Features />} />
-          <Route path="caracteristicas/:id" element={<EditFeature />} />
-          <Route path="agregar-caracteristica" element={<AddFeature />} />
-          <Route path="/administracion/listar-usuarios" element={<ListUsers />} />
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/administracion" element={<Dashboard />}>
+            <Route index element={<AddProducts />} />
+            <Route path="crear-producto" element={<CreateProduct />} />
+            <Route path="listar-productos" element={<ListProducts />} />
+            <Route path="editar-producto/:id" element={<EditProduct />} />
+            <Route path="caracteristicas" element={<Features />} />
+            <Route path="caracteristicas/:id" element={<EditFeature />} />
+            <Route path="agregar-caracteristica" element={<AddFeature />} />
+            <Route
+              path="/administracion/listar-usuarios"
+              element={<ListUsers />}
+            />
 
-        {/* Administración de Categorías */}
-          <Route path="administrar-categorias" element={<CategoryForm />} />
-          <Route path="listar-categorias" element={<CategoryList />} />
-
+            {/* Administración de Categorías */}
+            <Route path="administrar-categorias" element={<CategoryForm />} />
+            <Route path="listar-categorias" element={<CategoryList />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
--- src/views/Auth/ProtectRoute.jsx
+++ src/views/Auth/ProtectRoute.jsx
@@ -1,20 +1,41 @@
-import { useMemo } from 'react';
+import { useEffect,  useState } from 'react';
 import { Navigate, Outlet } from 'react-router';
 
 const ProtectedRoute = () => {
+  const [auth, setAuth] = useState(() => {
+    const userData = JSON.parse(localStorage.getItem('userGoTrip') || '{}');
+    return {
+      token: userData?.token,
+      role: userData?.user?.role,
+    };
+  });
+
+  
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const userData = JSON.parse(localStorage.getItem('userGoTrip') || '{}');
+      setAuth({
+        token: userData?.token,
+        role: userData?.user?.role,
+      });
+    };
+
+   
+    window.addEventListener('storage', handleStorageChange);
+
     
-    const { token, role } = useMemo(() => {
-        const userData = JSON.parse(localStorage.getItem('userGoTrip') || '{}');
-        return {
-          token: userData?.token,
-          role: userData?.user?.role,
-        };
-      }, []);
-    
-      if (!token) return <Navigate to="/login" replace />;
-      if (token && role !== 'ADMIN') return <Navigate to="/" replace />;
-      
-      return <Outlet />;
+    const observer = setInterval(handleStorageChange, 1000);
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+      clearInterval(observer);
+    };
+  }, []);
+
+  if (!auth.token) return <Navigate to="/login" replace />;
+  if (auth.token && auth.role !== 'ADMIN') return <Navigate to="/" replace />;
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
